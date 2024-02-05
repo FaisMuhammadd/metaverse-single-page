0f36300042e22f5a407ffd02b19c9d976a2f06b5
pandawaa <pallakangoding@gmail.com>
2024-02-05 14:58:19 +0800

    finsih About Section

--- sections/About.jsx
+++ sections/About.jsx
@@ -15,7 +15,7 @@ const About = () => (
       viewport={{ once: false, amount: 0.25 }}
       className={`${styles.innerWidth} mx-auto ${styles.flexCenter} flex-col`}
     >
-      <TypingText title="| About Metaverse" textStyles="text-center" />
+      <TypingText title="About Metaverse" textStyles="text-center" />
 
       <motion.p
         variants={fadeIn("up", "tween", 0.2, 1)}
