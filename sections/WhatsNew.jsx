ba9bdcd83607d9c92552fbf818394fa53ed280ae
pandawaa <pallakangoding@gmail.com>
2024-02-05 15:41:12 +0800

    finish whats new sections

--- sections/WhatsNew.jsx
+++ sections/WhatsNew.jsx
@@ -1,9 +1,44 @@
-'use client';
+"use client"
+
+import styles from "../styles"
+import { motion } from "framer-motion"
+import { fadeIn, planetVariants, staggerContainer } from "../utils/motion"
+import { NewFeatures, TitleText, TypingText } from "../components"
+import { newFeatures } from "../constants"
 
 const WhatsNew = () => (
-  <section>
-    What's new section
-  </section>
-);
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={fadeIn("right", "tween", 0.2, 1)}
+        className="flex-[0.95] flex justify-center flex-col"
+      >
+        <TypingText title="Whats new ?" />
+        <TitleText title={<>What's new about Metaverse?</>} />
+        <div className="mt-[48px] flex flex-wrap justify-between gap-[24px]">
+          {newFeatures.map((feature) => (
+            <NewFeatures key={feature.title} {...feature} />
+          ))}
+        </div>
+      </motion.div>
 
-export default WhatsNew;
+      <motion.div
+        variants={planetVariants("right")}
+        className={`flex-1 ${styles.flexCenter}`}
+      >
+        <img
+          src="/whats-new.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
+    </motion.div>
+  </section>
+)
+
+export default WhatsNew
