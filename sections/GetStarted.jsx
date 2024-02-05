40ed7c2819994aee569bcf01f8d2500064e7523e
pandawaa <pallakangoding@gmail.com>
2024-02-05 15:21:08 +0800

    finish Get Started section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -1,9 +1,49 @@
-'use client';
+"use client"
+
+import styles from "../styles"
+import { motion } from "framer-motion"
+import { fadeIn, planetVariants, staggerContainer } from "../utils/motion"
+import { StartSteps, TitleText, TypingText } from "../components"
+import { startingFeatures } from "../constants"
 
 const GetStarted = () => (
-  <section>
-    Get Started Section
-  </section>
-);
+  <section className={`${styles.paddings} relative z-10`}>
+    <motion.div
+      variants={staggerContainer}
+      initial="hidden"
+      whileInView="show"
+      viewport={{ once: false, amount: 0.25 }}
+      className={`${styles.innerWidth} mx-auto flex lg:flex-row flex-col gap-8`}
+    >
+      <motion.div
+        variants={planetVariants("left")}
+        className={`flex-1 $${styles.flexCenter}`}
+      >
+        <img
+          src="/get-started.png"
+          alt="get-started"
+          className="w-[90%] h-[90%] object-contain"
+        />
+      </motion.div>
 
-export default GetStarted;
+      <motion.div
+        variants={fadeIn("left", "tween", 0.2, 1)}
+        className="flex-[0.75] flex justify-center flex-col"
+      >
+        <TypingText title="How Metaverse Works" />
+        <TitleText title={<>Get started with just a few clicks</>} />
+        <div className="mt-[31px] flex flex-col max-w-[370px] gap-[24px]">
+          {startingFeatures.map((feature, index) => (
+            <StartSteps
+              key={feature}
+              number={`${index < 10 ? "0" : ""} ${index + 1}`}
+              text={feature}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </motion.div>
+  </section>
+)
+
+export default GetStarted
