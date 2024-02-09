db41d2fd69af201a50cad110215e558a096fde35
pandawaa <pallakangoding@gmail.com>
2024-02-09 09:43:02 +0800

    modified Feedback

--- sections/Feedback.jsx
+++ sections/Feedback.jsx
@@ -19,16 +19,16 @@ const Feedback = () => (
       >
         <div className="feedback-gradient" />
 
-        <div>
+        <div className="text-white">
           <h4 className="font-bold sm:text-[32px] text-[26px] sm:leading-[40.32px] leading-[36.32px] text-white">
-            Tompho
+            Tomphos
           </h4>
-          <p className="mt-[8px] font-normal sm:text-[18px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
+          <p className="mt-[8px] font-normal sm:text-[20px] text-[12px] sm:leading-[22.68px] leading-[16.68px] text-white">
             Founder Metaverse
           </p>
         </div>
 
-        <p className="mt-[24px] font-normal sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
+        <p className="mt-[24px] font-bold sm:text-[24px] text-[18px] sm:leading-[45.6px] leading-[39.6px] text-white">
           “With the development of today's technology, metaverse is very useful
           for today's work, or can be called web 3.0. by using metaverse you can
           use it as anything”
